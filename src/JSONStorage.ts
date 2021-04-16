aa7e3dfd2ed77f7aa3170d23302df8e984f44c69
Ahmzan <ahmzan.dev@gmail.com>
2021-04-16 23:35:29 +0700

    Update JSONStorage

--- src/JSONStorage.ts
+++ src/JSONStorage.ts
@@ -1,10 +1,15 @@
 import { readFileSync, writeFileSync } from 'fs';
+import { mkdirSync } from 'fs';
+import { dirname, resolve } from 'path';
 
-export class JSONStorage {
-  path: string;
-  data: Record<string, string>;
+export default class JSONStorage {
+  private path: string;
+  private data: Record<string, string>;
+  private dirname: string;
   constructor(path: string) {
-    this.path = path ? path : './data';
+    this.path = path ? path : './data.json';
+    this.dirname = dirname(resolve(this.path));
+    mkdirSync(this.dirname, { recursive: true });
     try {
       this.data = JSON.parse(readFileSync(this.path, 'utf-8'));
     } catch (e) {
@@ -13,12 +18,12 @@ export class JSONStorage {
     }
   }
 
-  getItem(key: string): string {
+  async getItem(key: string): Promise<string | null> {
     this.data = JSON.parse(readFileSync(this.path, 'utf-8'));
-    return this.data[key];
+    return this.data[key] ? this.data[key] : null;
   }
 
-  setItem(key: string, value: string): void {
+  async setItem(key: string, value: string): Promise<void> {
     this.data[key] = value;
     writeFileSync(this.path, JSON.stringify(this.data));
   }
