42c8927a80db80225c9496760bad143e8b0f7959
Ahmzan <ahmzan.dev@gmail.com>
2021-04-21 14:05:01 +0700

    Update example.ts and add example for js

--- examples/example.ts
+++ examples/example.ts
@@ -1,21 +1,22 @@
-import { Client, Filters } from '../src';
-import dotenv from 'dotenv';
+import { Client } from '../';
+import * as dotenv from 'dotenv';
+import { inspect } from 'util';
 dotenv.config();
 
-async function test() {
-  const api_hash = <string>process.env.API_HASH;
-  const api_id = <string>process.env.API_ID;
-  const bot = new Client(api_hash, api_id);
+const api_hash = <string>process.env.API_HASH;
+const api_id = <string>process.env.API_ID;
+const bot = new Client(api_hash, api_id);
 
-  bot.catch((err: any) => {
-    console.log('a error exist');
-    console.log(err);
-  });
-  // TODO: make on handler
-  bot.on(Filters.Text(), (upd) => {
-    upd.sendMessage('HI');
-  });
-  bot.start(2000);
-}
+bot.catch((err: any) => {
+  console.log('a error exist');
+  console.log(err);
+});
 
-test();
+bot.on('update', (upd) => {
+  console.log(inspect(upd, false, null, true));
+});
+
+bot.on('login', (user) => {
+  console.log('started as ', user.user.first_name);
+});
+bot.start();
