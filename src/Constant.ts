5c839fd1fd274bdb04cb8361447f83eaa8058a50
Ahmzan <ahmzan.dev@gmail.com>
2021-05-08 01:30:32 +0700

    Fixing package path

--- src/Constant.ts
+++ src/Constant.ts
@@ -1,4 +1,6 @@
 import { readFileSync } from 'fs';
-
-const pkg = JSON.parse(readFileSync('../package.json', 'utf-8'));
+import { resolve } from 'path';
+const root = __dirname.replace(/(lib|src)$/i, '');
+const pkgPath = resolve(root, 'package.json');
+const pkg = JSON.parse(readFileSync(pkgPath, 'utf-8'));
 export default { VERSION: pkg.version, APP_NAME: 'Tele.js' };
